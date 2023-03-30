a07ccb3c184903deb695f902569a8d3e5989d7f9
Theresa Summa <theresasumma@gmail.com>
2023-03-30 08:11:04 -0700

    making things build-able

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
-module.exports = {
-  mode: 'production',
-  module: {
-    rules: [{ test: /\.js$/, use: ['babel-loader'] }],
-  },
+const config = {
+    mode: 'production',
+    module: {
+      rules: [{ test: /\.js$/, use: ['babel-loader'] }],
+    },
 }
+
+export default config;
